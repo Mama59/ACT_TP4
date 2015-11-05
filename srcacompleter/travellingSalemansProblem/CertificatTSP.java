e3843580b1c576d870d8e215e943830e87606dd8
xstreel <contact@xavierstreel.com>
2015-11-05 21:54:52 +0100

    CertificatTSP

--- srcacompleter/travellingSalemansProblem/CertificatTSP.java
+++ srcacompleter/travellingSalemansProblem/CertificatTSP.java
@@ -1,47 +1,109 @@
 package travellingSalemansProblem;
 
 import java.util.Random;
+import java.util.Scanner;
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import classesPb.*;
 
 public class CertificatTSP implements Certificat{
 // A completer
-	List<Character> liste;
+	private List<Character> liste;
+	private List<Character> lstSaisie; // originale
+	private List<Character> valeursPossibles;
+	private TSP tsp;
+	
 	public CertificatTSP(TSP tsp){
-		liste = new ArrayList<Character>();
+		this.tsp = tsp;
+		this.liste = new ArrayList<Character>(tsp.nbVilles);
+		this.lstSaisie = new ArrayList<Character>(tsp.nbVilles);
 	}
 	
 	@Override
 	public void saisie() {
-		// TODO Auto-generated method stub
-		
+		Scanner sc = new Scanner(System.in);
+		String saisie = sc.nextLine();
+		while (saisie.length() != this.tsp.nbVilles) {
+			System.out.println("Vous devez entrer exactement " + this.tsp.nbVilles + " caractères");
+			saisie = sc.nextLine();
+		}
+		for (int i = 0; i < saisie.length(); i++) {
+			this.liste.add(saisie.charAt(i));
+			this.lstSaisie.add(saisie.charAt(i));
+		}
+		this.valeursPossibles = new ArrayList<Character>();
+		this.liste.forEach(f -> {
+			if (!this.valeursPossibles.contains(f)) {
+				this.valeursPossibles.add(f);
+			}
+		});
 	}
+	
 	@Override
 	public void display() {
-		// TODO Auto-generated method stub
-		System.out.println();
+		System.out.println(liste.toString());
 	}
+	
 	@Override
 	public void alea() {
-		// TODO Auto-generated method stub
-		
+		List<Character> newCertif = new ArrayList<Character>();
+		this.liste.forEach(f -> newCertif.add(this.valeursPossibles.get((int) (Math.random() * this.valeursPossibles.size()))));	
+		this.liste = newCertif;		
 	}
 	@Override
+	// reset = premiere valeur saisie ou premiere valeur possible ?
 	public void reset() {
 		// TODO Auto-generated method stub
-		
+		for (int i = 0; i < this.liste.size(); i++) {
+			this.liste.set(i, this.lstSaisie.get(i));
+		}
 	}
 	@Override
 	public boolean estDernier() {
-		// TODO Auto-generated method stub
-		return false;
+		System.out.println(this.valeursPossibles.size());
+		for (int i = 0; i < this.liste.size(); i++) {
+			if (this.liste.get(i) != this.valeursPossibles.get(this.valeursPossibles.size() - 1)) {
+				return false;
+			}
+		}
+		return true;
 	}
 	@Override
 	public void suivant() {
-		// TODO Auto-generated method stub
+		if (this.estDernier()) {
+			return;
+		}
 		
+		boolean exit = false;
+		// on parcourt la liste en commençant par le dernier caract
+		for (int i = this.liste.size() - 1; i >= 0; i--) {
+			if (exit) {
+				return;
+			}
+			// si pas le caract max, on ncrémente le caract
+			if (this.liste.get(i) != this.valeursPossibles.get(this.valeursPossibles.size() - 1)) {
+				this.liste.set(i, this.valeursPossibles.get(this.valeursPossibles.indexOf(this.liste.get(i)) + 1));
+				exit = true;
+			}
+			// sinon, c'ets le dernier caract possible, on insère le premier et passe à la ville precédente
+			else {
+				this.liste.set(i, this.valeursPossibles.get(0));
+			}
+		}	
+	}
+
+	@Override
+	public boolean haveDoublons() {
+		List<Character> lst = new ArrayList<Character>();
+		for (int i = 0; i < this.liste.size(); i++) {
+			if (lst.contains(this.liste.get(i))) {
+				return true;
+			}
+			lst.add(this.liste.get(i));
+		}
+		return false;
 	}
 
 }
